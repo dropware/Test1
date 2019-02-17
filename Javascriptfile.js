94d3ff24c3aaaba3cee3a37914d2b558ee69d6f6
jlvelardek <jlvelardek@gmail.com>
2019-02-16 19:25:01 -0600

    Wooooo!

---
+++ Javascriptfile.js
@@ -0,0 +1,7 @@
+function buttonFunction() {
+  //console.log("Login button pressed")
+  alert("Succesful Login!");
+  object.onclick = function(){buttonFunction};
+}
+
+//object.onclick = function(){buttonFunction};
